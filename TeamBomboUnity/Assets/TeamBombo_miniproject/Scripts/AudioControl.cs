58624a17e1ce4ae8498353c2d6c15367277b8ff9
tushars92 <tusharsupe@gatech.edu>
2014-12-01 17:53:06 -0500

    - Added Awesomeness

---
+++ TeamBomboUnity/Assets/TeamBombo_miniproject/Scripts/AudioControl.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioControl : MonoBehaviour {
+	public float HeadAlarmAngle = 30f;
+	public AudioClip Startup, HeadTurn;
+
+	// Use this for initialization
+	void Start () {
+		audio.clip = Startup;
+		audio.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ((transform.localEulerAngles.y > HeadAlarmAngle) && (transform.localEulerAngles.y < (360f - HeadAlarmAngle))
+		    && (FPSInputController.instance.motor.movement.velocity.magnitude > 5)) {
+			audio.clip = HeadTurn;
+			if (!audio.isPlaying) audio.Play();	
+			Debug.Log("Look Ahead Dumbass!");
+		}
+	}
+}
---
+++ TeamBomboUnity/Assets/TeamBombo_miniproject/Scripts/Intersection.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Intersection : MonoBehaviour {
+	public AudioClip[] RouteClip = new AudioClip[5];
+	public GameObject RouteObject;
+	RectTransform Route;
+	AudioSource RouteSource;
+	int RouteNumber;
+
+	// Use this for initialization
+	void Start () {
+		Route = RouteObject.GetComponent("Transform") as RectTransform;
+		RouteSource = RouteObject.GetComponent("AudioSource") as AudioSource;
+		RouteNumber = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ((transform.position.z < -55) && (transform.position.z > -85) && 
+						(transform.position.x > 35) && (transform.position.x < 115) && (Input.GetButton ("Fire1"))) {
+			RouteSource.clip = RouteClip[RouteNumber];
+			RouteSource.Play ();
+			if (RouteNumber == 4)	RouteNumber = 0;
+			else RouteNumber += 1;
+		}
+	}
+
+}
